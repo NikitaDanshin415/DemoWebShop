ccce4cc9f1129597f2c0a577f2898e1f05bf56a3
Nikita <danshinnp@yandex.ru>
2022-04-13 18:59:33 +0500

    created Setting Test class for make base configuration

---
+++ src/test/java/com/demowebshop/apiui/config/SettingTest.java
@@ -0,0 +1,43 @@
+package com.demowebshop.apiui.config;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import com.demowebshop.apiui.config.app.App;
+import com.demowebshop.apiui.config.webdriver.WebDriverProvider;
+import io.qameta.allure.selenide.AllureSelenide;
+import io.restassured.RestAssured;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class SettingTest {
+    public void configure(){
+        Configuration.browser = WebDriverProvider.config.getBrowser();
+        Configuration.baseUrl = App.config.getWebUrl();
+        RestAssured.basePath = App.config.getApiUrl();
+
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        ChromeOptions chromeOptions = new ChromeOptions();
+
+        chromeOptions.addArguments("--no-sandbox");
+        chromeOptions.addArguments("--disable-infobars");
+        chromeOptions.addArguments("--disable-popup-blocking");
+        chromeOptions.addArguments("--disable-notifications");
+        chromeOptions.addArguments("--lang=en-en");
+
+        if (WebDriverProvider.config.isRemote()) {
+            capabilities.setCapability("enableVNC", true);
+            capabilities.setCapability("enableVideo", true);
+            Configuration.remote = "https://"
+                + WebDriverProvider.config.getRemoteLogin()
+                + ":" + WebDriverProvider.config.getRemotePassword()
+                + "@"
+                + WebDriverProvider.config.getRemoteDriverUrl()
+                + "/wd/hub";
+        }
+
+        capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
+        Configuration.browserCapabilities = capabilities;
+    }
+}
--- src/test/java/com/demowebshop/apiui/tools/ui/helpers/BaseTest.java
+++ src/test/java/com/demowebshop/apiui/tools/ui/helpers/BaseTest.java
@@ -1,16 +1,11 @@
 package com.demowebshop.apiui.tools.ui.helpers;
 
-import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import com.demowebshop.apiui.config.app.App;
-import com.demowebshop.apiui.config.webdriver.WebDriverProvider;
+import com.demowebshop.apiui.config.SettingTest;
 import com.demowebshop.apiui.tools.Attacher;
 import io.qameta.allure.selenide.AllureSelenide;
-import io.restassured.RestAssured;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
@@ -18,28 +13,17 @@ public class BaseTest {
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.browser = WebDriverProvider.config.getBrowser();
-        Configuration.baseUrl = App.config.getWebUrl();
-        RestAssured.basePath = App.config.getApiUrl();
-
-        SelenideLogger.addListener("allure", new AllureSelenide());
-
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        ChromeOptions chromeOptions = new ChromeOptions();
-
-        if (WebDriverProvider.config.isRemote()) {
-            capabilities.setCapability("enableVNC", true);
-            capabilities.setCapability("enableVideo", true);
-            Configuration.remote = WebDriverProvider.config.getRemoteDriverUrl();
-        }
+       new SettingTest().configure();
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
     @AfterEach
     void addAttachments() {
-        Attacher.screenshotAs("Screenshot");
+        Attacher.screenshotAs("Last screenshot");
         Attacher.pageSource();
         Attacher.browserConsoleLogs();
         Attacher.addVideo();
+
         closeWebDriver();
     }
 
